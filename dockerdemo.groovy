8fe781530a09b2c63c7fa8d861b4eb6cb39dd67c
manshaaazar <46319250+manshaaazar@users.noreply.github.com>
2019-10-25 22:44:31 +0500

    Add files via upload

---
+++ dockerdemo.groovy
@@ -0,0 +1,25 @@
+job('NodeJs Docker Example'){
+    scm{
+        git('git://github.com/manshaaazar/demo-node.git'){ node ->
+        node / gitConfigName('manshaaazar')
+        node / gitConfigEmail('manshaehsan000@gmail.com')
+
+    }
+    triggers{
+        scm('H/5 * * * *')
+    }
+    wrappers{
+        nodejs('nodejs')
+    }
+    steps{
+        dockerBuildAndPublish{
+            repositoryName('manshaehsan/jenkins-tutorials')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprinters(false)
+            skipDecorate()
+        }
+    }
+}
